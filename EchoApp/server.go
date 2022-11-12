2611ddb7d5c13f7583776923a7a5ac463f426a95
Nicolás Bacquet <nibata@gmail.com>
2022-11-12 09:36:32 -0300

    Se agrega documentación Swagger

---
+++ EchoApp/server.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"net/http"
+
+	_ "TestGO/docs/EchoApp"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+// @title API ECHO de prueba
+// @version 1.0
+// @description Este es un ejemplo sencillo utilizando Echo para crear una api.
+// @host localhost:3000
+// @BasePath /
+// @schemes http
+
+type Message struct {
+	Msg string `json:"message"`
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// Routes
+	e.GET("/", Welcome)
+	e.GET("/docs/*", echoSwagger.WrapHandler)
+	e.GET("/usopathparams/:value", UsoParametros)
+	e.GET("/usoqueryparams", UsoQuery)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+// Welcome godoc
+// @Summary Muestra mensaje de bienvenida.
+// @Description Endpoint básico que muestra mensaje de bienvenida en el servidor.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
+func Welcome(c echo.Context) error {
+	return c.JSON(http.StatusOK, Message{
+		Msg: "Bienvenido",
+	})
+}
+
+// UsoPath godoc
+// @Summary Muestra valor de variable especificada.
+// @Description Endpoint que muestra el valor de la variable especificada por path.
+// @Param value path string true "Valor que se mostrará en la respuesta"
+// @Tags UsoParametros
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router /usopathparams/{value} [get]
+func UsoParametros(c echo.Context) error {
+	params := c.Param("value")
+	return c.JSON(http.StatusOK, Message{
+		Msg: "El valor entregado por Path Params a 'value' es: " + params,
+	})
+}
+
+// UsoQuery godoc
+// @Summary Muestra valor de la variable value especificada.
+// @Description Endpoint que muestra el valor de la variable 'value' especificada por query.
+// @Param value query string true "Valor que se mostrará en la respuesta"
+// @Tags UsoQuery
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router /usoqueryparams [get]
+func UsoQuery(c echo.Context) error {
+	query := c.QueryParam("value")
+	return c.JSON(http.StatusOK, Message{
+		Msg: "El valor entregado por Query Params a 'value' es: " + query,
+	})
+}
--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # TestGO
 proyecto para experimentar con golang
 
+Para este proyecto me guío por las guías que están en (en especial para la instalación de Swagger):
+ 
+ - https://github.com/swaggo/echo-swagger
+ - https://medium.com/geekculture/tutorial-generate-swagger-specification-and-swaggerui-for-echo-go-web-framework-3ac33afc77e2
+
 De momento se usa se está utilizando la librería `ECHO` para crear APIs.
 
 El proyecto cuenta con tres endpoints:
 
- - `[::]:3000` ccon un mensaje de bienvenida simple y plano.
+ - `[::]:3000` con un mensaje de bienvenida simple y plano.
+ - `[::]:3000/docs/index.html` contiene la documentación SWAGGER.
  - `[::]:3000/usopathparams/{ALGUN_VALOR}` que muestra como se utiliza un endpoint con Path Params.
  - `[::]:3000/usoqueryparams?value={ALGUN_VALOR}` que muestra como se utiliza un endpoint con Query Params.
 
@@ -13,7 +19,10 @@ El proyecto cuenta con tres endpoints:
 `go mod vendor`
 
 Y finalmente se inicia el proyecto con:
-`go run sserver.go`
+`go run server.go`
+
+Para actualizar la documentación:
+`swag init -g EchoApp/server.go --output docs/EchoApp`
 
 Proximos pasos:
  - estructurar mejor el proyecto de manera que sea editable de mejor manera.
